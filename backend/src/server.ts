bb123911c0010e50d3058bfda9914b267e1924bd
Steven Ayala <stevengilbertayala@gmail.com>
2025-11-22 18:31:40 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
